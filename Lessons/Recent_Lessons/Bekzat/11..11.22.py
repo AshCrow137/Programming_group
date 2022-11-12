297929237778eb609a1289f014a26eea53a0561a
Bek1zat <kurbanbajbekzat938@gmail.com>
2022-11-12 16:13:58 +0600

    12.11.22.py

--- Lessons/Recent_Lessons/Bekzat/11..11.22.py
+++ Lessons/Recent_Lessons/Bekzat/11..11.22.py
@@ -12,7 +12,7 @@ def draw_a_card():
         card = None
     return card
 
-def update_score(score):
+def update_score(score,player_score):
     current_score = player_score + score
     print(f'У вас {current_score} очков')
     return current_score
@@ -37,27 +37,77 @@ def b_win(score):
     elif score < 21:
         return None
 
-deck = [1,2,3,4,5,6,78,9,10,11]*4
+def setup_players():
+    num_players = int(input('Введите количиство играков'))
+    players = []
+    for num1 in range(1,num_players + 1):
+        player = {
+            'name':f'player_N_{num1}',
+            'score' : 0,
+            'winner' :False
+        }
+        players.append(player)
+    print(players)
+    return players
+
+def find_winner(players_to_find):
+    for player in players_to_find:
+        if player['winner'] == True:
+            winner = player
+            return winner
+
+    if winner == None:
+        p_winners = []
+        for player in players_to_find:
+            score = player['score']
+            if score > 20:
+                pass
+            else:
+                 p_winners.append(player)
+        p_winners.sort()
+
+        print(p_winners)
+        win_score = p_winners[-1]
+        for player in players_to_find:
+            if player['score'] == win_score:
+                winner = player
+                return player
+
+deck = [1,2,3,4,5,6,7,8,9,10,11]*4
 random.shuffle(deck)
 
 
 
-b_continue = True
-player_score = 0
-while b_continue:
 
-    card = draw_a_card()
 
-    if draw_a_card() != None:
-        print(card)
-        player_score = update_score(card)
-        win= b_win(player_score)
-        if b_win(player_score) == True:
+players = setup_players()
+
+for active_player in players:
+    print('Активный игрок: ',active_player)
+    b_continue = True
+
+    while b_continue:
+
+        card = draw_a_card()
+
+        if draw_a_card() != None:
+            print(card)
+            active_player['score'] = update_score(card,active_player['score'])
+            win= b_win(active_player['score'])
+            if win == None:
+                pass
+            elif win == False:
+                break
+            elif win == True:
+                active_player['winner'] = True
+                break
+        else:
             break
-        elif b_win(player_score) == False:
-            break
-    else:
-        break
-    b_continue = declision()
+        b_continue = declision()
+
+
+print(players)
+
+print (find_winner (players))
 
 print('Игра окончена')
