acd96cc8c2f240187e4c1d1c1e3ec8c030fd7dbc
Bek1zat <kurbanbajbekzat938@gmail.com>
2022-11-17 19:04:08 +0600

    17.11.22.py

--- Lessons/Recent_Lessons/Bekzat/11.11.22.py
+++ Lessons/Recent_Lessons/Bekzat/11.11.22.py
@@ -1,22 +1,10 @@
-# tre = 0 
 
-# for use in  range(1,101):
-#     tre = tre + use 
-
-#     print(tre)
-
-
-# tre = 0 + 1  
-# tre = 1 + 2
-# tre = 3 + 3
-# tre = 6 + 4
-# tre = 10 + 5
 
 
 kit = 'Привет Бекзат'
 
 print('Привет')
-print(kit)
+#print(kit)
 
 ter = 8
 lot = 4
@@ -26,5 +14,5 @@ print(ter)
 print(lot)
 print(yor)
 
-yor = ter * lot
+yor = ter * lot 
 print(yor,ter,lot)
