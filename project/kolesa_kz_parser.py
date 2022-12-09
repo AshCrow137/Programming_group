7ba582fafe5ac3ade2c8ee148cecb42cf656dbdb
Bek1zat <kurbanbajbekzat938@gmail.com>
2022-12-09 19:53:26 +0600

    ksjeffj

--- project/kolesa_kz_parser.py
+++ project/kolesa_kz_parser.py
@@ -7,7 +7,7 @@ from lxml import etree
 
 
 def parse_kolesa_kz(car_mark,car_condition,car_model,car_location,car_body):
-    URL = f'https://kolesa.kz/cars/{car_mark}{car_condition}{car_model}{car_location}/?auto-car-grbody={car_body}&sort_by=price-asc'
+    URL = f'https://kolesa.kz/cars/{car_mark}{car_condition}{car_model}{car_location}?auto-car-grbody={car_body}&sort_by=price-asc'
     print(URL)
     get_html_text = requests.get(URL)
     print(get_html_text.status_code)
@@ -19,17 +19,16 @@ def parse_kolesa_kz(car_mark,car_condition,car_model,car_location,car_body):
             content_ad = tree.xpath(f'//*[@class="a-list"]/div[{i}]/div/div[2]/div[1]/h5/a/@href')
             if content_ad:
                 result_list.append('https://kolesa.kz/'+content_ad[0])
-        #content = tree.xpath('//*[@id="advert-145743989"]/div[2]/div[1]/h5/a/@href')
-        #'//*[@id="advert-145984145"]/div[2]/div[1]/h5/a'
         return result_list
     else:
         return 'Не удалось подключиться к сайту'
 
-car_mark = ''
-car_condition = 'novye-avtomobili/'
-car_model = ''
-car_location = 'astana/'
-car_body = 2
 
-result =parse_kolesa_kz(car_mark,car_condition,car_model,car_location,car_body)
-print(result)
+# car_mark = ''
+# car_condition = 'novye-avtomobili/'
+# car_model = ''
+# car_location = 'astana/'
+# car_body = 2
+
+# result = parse_kolesa_kz(car_mark,car_condition,car_model,car_location,car_body)
+# print(result)
