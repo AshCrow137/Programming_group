2f9b03899f3bff68766cfaa8fe01512d6c4f9f8e
Bek1zat <kurbanbajbekzat938@gmail.com>
2022-12-13 20:46:09 +0600

    sgdghgs

--- Project/kolesa_telebot.py
+++ Project/kolesa_telebot.py
@@ -12,6 +12,9 @@ dict_of_models = {'Toyota':['Camry','Land Cruiser', 'Prado,Land','Cruiser','RAV4
 'Volkswargen':['Passat','Golf','Polo','Vento','Transporter','Jetta','Touareg','Tiguan']
 }
 
+dict_of_location = {'location':['Алматы', 'Астана', 'Караганда', 'Шымкент', 'Актобе', 'Костанай', 'Атырау']
+}
+
 
 def create_keybord(list_of_buttons):
     markup = types.ReplyKeyboardMarkup(resize_keyboard= True)
@@ -39,16 +42,29 @@ def reply_to_all_message(message):
             choose_model(message,model)
 
 
+
+
+
+
 def choose_model(message):
 
     markup = create_keybord()
-    bot.send_message(message.chat.id,'Локация Астана')
+    bot.send_message(message.chat.id,'Выбирите локацию ')
 
 
 @bot.message_handler(commands=['location','Location'])
 def send_location_message(message):
-    
+    markup = create_keybord(['location'])
+    bot.send_message(message.chat.id,'Выбирите локацию машины', reply_location= location)
 
+
+@bot message_handler(content_types=['text'])
+def reply_to_all_message(message):
+    for location in dict_of_location.keys():
+        if message.text == location:
+            choose_model(message,location)
+
+    
 
 while True:
     try:
