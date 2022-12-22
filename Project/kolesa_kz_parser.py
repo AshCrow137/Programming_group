5955f81cc05995d977ea062a30510d6971f08f51
Alikhan05 <alihanmursalimov79@gmail.com>
2022-12-22 19:44:36 +0600

    parser
    car informations

--- Project/kolesa_kz_parser.py
+++ Project/kolesa_kz_parser.py
@@ -5,39 +5,28 @@ import json
 import lxml.html
 from lxml import etree
 
-def car_informations(): 
-    car_characteristic = {
-        'город': None,
-        "поколение":None,
-        "кузов":None,
-        "объем двигателя":None,
-        "пробег":None,
-        "коробка передач":None,
-        "привод":None,
-        "руль":None,
-        "цыет":None,
-        "растаможен в Казахстане":None
-    }
-    URL = result_list[0]
-    print(URL)
-    get_html_text = requests.get(URL)
-    print(get_html_text.status_code)
+def car_informations(url):   
+    car_information_values = []
+    get_html_text = requests.get(url)
+
     if get_html_text.status_code == 200:
         tree = lxml.html.document_fromstring(get_html_text.text)
         list_of_info=tree.xpath('//*[@class="offer__parameters"]/dl')
         for i in range(1,len(list_of_info)+1):
             car_info = tree.xpath(f'//*[@class="offer__parameters"]/dl[{i}]/dd/text()')
+            car_information_values.append(car_info[0])
+        info_dict = zip(car_characteristics ,car_information_values)
+        print(dict(info_dict))
 
-            print(car_info[0])
-        
- 
+
+
 def parse_kolesa_kz(car_mark,car_condition,car_model,car_location,car_body):
     URL = f'https://kolesa.kz/cars/{car_mark}{car_condition}{car_model}{car_location}?auto-car-grbody={car_body}&sort_by=price-asc'
-    print(URL)
+    #print(URL)
     get_html_text = requests.get(URL)
-    print(get_html_text.status_code)
+    #print(get_html_text.status_code)
     if get_html_text.status_code == 200:
-        global result_list
+
         result_list = []
         tree = lxml.html.document_fromstring(get_html_text.text)
         list_of_ads = tree.xpath('//*[@class="a-list"]/div')
@@ -49,6 +38,7 @@ def parse_kolesa_kz(car_mark,car_condition,car_model,car_location,car_body):
     else:
         return 'Не удалось подключиться к сайту'
 
+car_characteristics= ['город','покление','кузов','объем двигателя','коробка передач','привод','руль','растаможен в Казахстане']
 
 car_mark = ''
 car_condition = 'novye-avtomobili/'
@@ -59,6 +49,6 @@ car_body = 2
 result = parse_kolesa_kz(car_mark,car_condition,car_model,car_location,car_body)
 print(result)
 
-car_informations()
+car_informations(result[0])
 
 
